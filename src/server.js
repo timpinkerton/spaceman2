c7de46086d0044a0c8a22025ed7efa6e2e413da9
Tim Pinkerton <timpinkerton@mail.com>
2017-06-27 22:31:55 -0400

    updated server with publicPath and added index.html

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 //importing required modules
 const express = require('express');
 const config = require('./config');
+const path = require('path');
 
 const app = express(); 
 
-app.use(function(req, res, next){
-    res.send('Hello Whirled!');
-});
+//path.resolve: a method that resolves to an absolute path
+const publicPath = path.resolve(__dirname, './public');
+//static is the middleware function that serves static files
+app.use(express.static(publicPath));
 
 
 app.use('/doc', function(req, res, next) {
   res.end(`Documentation http://expressjs.com/`);
+});
+
+app.use(function(req, res, next){
+    res.send('Hello Whirled!');
 });
 
 
